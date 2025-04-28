727bda8dc380e9b5f9833f7233ba1cca0d1e6952
SunilMandava <sunilmandava8@gmail.com>
2025-04-28 12:38:15 +0530

    Add files via upload

---
+++ Arithmetic operartions.py
@@ -0,0 +1,8 @@
+first_number=int(input('Enter the value of first_number ='))
+second_number=int(input('Enter the value of second_number=',))
+
+print("Addition :",first_number + second_number)
+print("subtraction :",first_number -second_number)
+print("multiplication :",first_number *second_number)
+print("division:",first_number /second_number)
+
